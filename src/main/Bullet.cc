445935684a650c66ad4475a778e41c1044e57dcb
Daly Barron <dalygbarron@gmail.com>
2020-03-28 14:15:12 +0000

    adding constructor for protobullet

--- src/main/Bullet.cc
+++ src/main/Bullet.cc
@@ -1,5 +1,12 @@
 #include "Bullet.hh"
 
+Bullet::Prototype Prototype(sf::IntRect rat, float radius):
+    rat(rat),
+    radius(radius)
+{
+    // does nothign.
+}
+
 Bullet::Bullet(Bullet::Prototype const &prototype, Actor const &parent):
     Mob((float)prototype.rat.width / 2),
     prototype(prototype), 
