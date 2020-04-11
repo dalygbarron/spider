544a9f0746dd7bb00a3c2a9a010e4efe7c8073ab
Daly Barron <dalygbarron@gmail.com>
2020-04-11 12:04:24 +0100

    boat level making

---
+++ share/boat.lua
@@ -0,0 +1,64 @@
+local boat = {}
+
+local xmlConversions = {
+    union = function(item, x, y)
+        local accumulation = ""
+        for i, content in ipairs(item.content) do
+            accumulation = accumulation..boat.xml(content, x, y)
+        end
+        return accumulation
+    end,
+    distribution = function(item, x, y)
+        local accumulation = ""
+        for i = 1, item.n, 1 do
+            -- TODO: proper distribution
+            local nX = x + math.random() * item.deviance
+            local nY = y + math.random() * item.deviance
+            accumulation = accumulation..boat.xml(item.content, nX, nY)
+        end
+        return accumulation
+    end,
+    mob = function(item, x, y)
+        return string.format(
+            "<mob name=\"%s\" x=\"%f\" y=\"%f\" />",
+            item.name,
+            x,
+            y
+        )
+    end
+}
+
+function boat.union(...)
+    return {
+        type = "union",
+        content = {...}
+    }
+end
+
+function boat.distribution(n, deviance, content)
+    return {
+        type = "distribution",
+        n = n,
+        deviance = deviance,
+        content = content
+    }
+end
+
+function boat.mob(name)
+    return {
+        type = "mob",
+        name = name
+    }
+end
+
+function boat.xml(item, x, y)
+    if x == nil then x = 0 end
+    if y == nil then y = 0 end
+    if xmlConversions[item.type] then
+        return xmlConversions[item.type](item, x, y)
+    else
+        _logError(string.format("no boating type '%s'", item.type))
+    end
+end
+
+return boat
