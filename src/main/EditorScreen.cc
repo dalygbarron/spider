110fe0ef8cb1326521260fe9afaeb595b3d79ff5
daly barron <dalygbarron@gmail.com>
2020-12-01 13:12:27 +0000

    some structure

--- src/main/EditorScreen.cc
+++ src/main/EditorScreen.cc
@@ -6,20 +6,43 @@ EditorScreen::EditorScreen(Core &core): Screen(core) {
 
 void EditorScreen::update(float delta, sf::RenderWindow &window) {
     ImGui::SFML::Update(window, sf::seconds(delta));
-    if (ImGui::BeginMenuBar()) {
+    if (ImGui::BeginMainMenuBar()) {
         if (ImGui::BeginMenu("File")) {
-            ImGui::MenuItem("Load Game");
-            ImGui::MenuItem("Save Game");
+            if (ImGui::MenuItem("New Game")) {
+                printf("newing\n");
+            }
+            if (ImGui::MenuItem("Load Game")) {
+                printf("loading\n");
+            }
+            if (ImGui::MenuItem("Save Game")) {
+                printf("saving\n");
+            }
             ImGui::EndMenu();
         }
-        if (ImGui::BeginMenu("Tabs")) {
-            ImGui::MenuItem("Close All But Current");
-            ImGui::MenuItem("Open File Browser");
-            ImGui::MenuItem("Open Game Settings");
+        if (ImGui::BeginMenu("View")) {
+            if (ImGui::MenuItem("Close All But Current Tab")) {
+                printf("closing things\n");
+            }
             ImGui::EndMenu();
         }
-        ImGui::Text("Hnelli");
+        ImGui::EndMainMenuBar();
     }
+    if (!ImGui::Begin("Editor")) {
+        ImGui::End();
+        return;
+    }
+    if (ImGui::BeginTabBar("tabs")) {
+        if (ImGui::BeginTabItem("start.plx")) {
+            ImGui::Text("Hnelli");
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("cirno.pex")) {
+            ImGui::Text("Idiota");
+            ImGui::EndTabItem();
+        }
+        ImGui::EndTabBar();
+    }
+    ImGui::End();
 }
 
 void EditorScreen::draw(sf::RenderTarget &target, int top) const {
